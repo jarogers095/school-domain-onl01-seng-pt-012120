04c99e5fd2fc787a5b4c4c6491185fd0190981d1
James Rogers <jarogers095@gmail.com>
2020-02-05 08:05:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,5 +3,18 @@ class School
   
   def initialize(school_name)
     @name = school_name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    @roster[student_grade] << student_name
+  end
+  
+  def grade(class_grade)
+    return @roster[class_grade]
+  end
+  
+  def sort()
+    return @roster
   end
 end
